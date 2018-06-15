ffc601f8f919e4f0a83121d4b7838ffdbb1cdb0b
Benjamin Jones <bljones36@gmail.com>
2018-06-15 01:38:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
+<<<<<<< HEAD
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days) {
+=======
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday) {
+>>>>>>> dac26b9bfd29ed92367f09a71bb3ee8169ac756e
   return `It's ${days} days until ${holiday}!`
 }
